327bd985f0d721734aef9a3fe29431dfec6d5d20
Brad Vorjohan <bhvorjo@gmail.com>
2020-05-23 22:58:46 -0400

    detecting jumpball command and finding users

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
 // I run in the background, can persist for the duration of the browser session,
 // and am useful for storing state, listening for browser events, etc.
-// this is where you would store, update, load, etc. your personal settings. 
+// this is where you would store, update, load, etc. your personal settings.
+// app id: cbeplaboifjfojegcmikljkpdnbppeoh
+// oauth client id: 848278120749-c4fn6cltlcnt70e52j9bio7q9k5a3dmn.apps.googleusercontent.com
+// "oauth2": {
+//   "client_id": "665859454684.apps.googleusercontent.com",
+//   "scopes": [
+//     "https://www.googleapis.com/auth/userinfo.email"
+//   ]
+// }
+console.log("background.js loaded")
+// chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+//   // Use the token.
+//   console.log(token)
+// });
--- content.js
+++ content.js
@@ -2,8 +2,62 @@
 
 
 // test to be sure is working an filter domains correctly
-alert("Hello")
+// alert("Hello")
 
+let chatInput = null;
+let chatButton = null;
+let intervalToSetWatcherForTextInput = null;
+let chatHistory = null;
+let peopleButton = null;
 
-// TODO: only begin when meeting starts (not on page load)
-// TODO: figure out muting people
+const runJumpball =  () => {
+  console.log("running jumpball!")
+  console.log("picking someone at random...")
+  peopleButton = document.querySelector(".ThdJC.kaAt2.c0XF8e.s7PhZd.Z9zn3b.sUgV6e")
+  // console.log(peopleButton)
+  if (!!peopleButton) {
+    peopleButton.click()
+    setTimeout(() => {
+      peoplesNames = document.querySelectorAll(".cS7aqe.NkoVdd")
+      // peoplesNames = document.querySelector
+      console.log("these are the people...")
+      peoplesNames.forEach((element, i) => {
+        console.log(element.innerText)
+      });
+
+    },500)
+  }
+}
+
+let intervalToCheckIfJoined = setInterval(() => {
+  const checkIfJoinedElement = document.querySelector(".Jrb8ue")
+  if (!!checkIfJoinedElement) {
+    console.log("joined meeting!")
+    chatButton = document.querySelector(".NPEfkd.RveJvd.snByac")
+
+    intervalToSetWatcherForTextInput = setInterval(() => {
+      chatInput = document.querySelector(".KHxj8b.tL9Q4c");
+      if (!!chatInput) {
+        chatHistory = document.querySelector(".oIy2qc")
+        // console.log(chatInput)
+        if (!!chatHistory) {
+          if (chatHistory.innerText === '!jumpball') {
+            clearInterval(intervalToSetWatcherForTextInput)
+            runJumpball()
+          }
+
+        }
+        // chatInput.addEventListener('input',(event) => console.log(event))
+      }
+    },1000)
+    clearInterval(intervalToCheckIfJoined)
+  }
+},1000)
+
+// let joinNowButton = document.querySelector(".e19J0b.CeoRYc");
+// console.log(joinNowButton)
+// joinNowButton.addEventListener("click",() => console.log("was clicked"))
+
+// let chatInput = document.querySelector(".KHxj8b.tL9Q4c");
+// console.log(chatInput)
+// chatInput.addEventListener('input',(event) => console.log(event))
